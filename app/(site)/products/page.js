60486840bd82f4db119130edcb52a66503875b1d
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-30 22:33:33 +0600

    change products page header bg color

--- app/(site)/products/page.js
+++ app/(site)/products/page.js
@@ -21,12 +21,12 @@ const ProductsPage = async ({ searchParams }) => {
 
   return (
     <main>
-      <div className="w-full bg-[#16A34A] text-white py-12 md:py14 px-2 text-center border-b border-green-700/30">
+      <div className="w-full bg-linear-to-br from-emerald-900 via-green-950 to-stone-900 text-white py-12 md:py14 px-2 text-center border-b border-green-700/30">
         <h1 className="text-3xl md:text-4xl font-bold tracking-tight mb-2">
           All Products
         </h1>
         <p className="text-sm md:text-lg text-green-100/90 font-light leading-relaxed">
-          Discover beautiful, locally-sourced bamboo, cane, and jute heritage
+          Discover beautiful, locally-sourced bamboo, cane, and jute heritage <br/>
           crafts handmade with precision by our trusted traditional artisans.
         </p>
       </div>
