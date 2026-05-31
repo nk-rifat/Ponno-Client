67e98d4b6160bf1189b803e777dc95b45c868312
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-05-31 13:32:43 +0600

    complete the featured products component

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -1,4 +1,5 @@
 import CategoryGrid from "@/components/home/category/CategoryGrid";
+import FeaturedProducts from "@/components/home/featured-products/FeaturedProducts";
 import HeroSlider from "@/components/home/hero/HeroSlider";
 
 export default function HomePage() {
@@ -6,6 +7,7 @@ export default function HomePage() {
     <>
     <HeroSlider />
     <CategoryGrid/>
+    <FeaturedProducts/>
     </>
   );
 }
---
+++ components/home/featured-products/FeaturedProducts.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import ProductCard from "@/components/products/ProductCard";
+import { getFeaturedProducts } from "@/lib/products";
+
+const FeaturedProducts = async () => {
+  const { data: products } = await getFeaturedProducts();
+
+  return (
+    <section className="max-w-7xl mx-auto px-4 md:px-8 py-12">
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <h2 className="text-3xl font-bold text-emerald-800">New Products</h2>
+          <p className="text-gray-500 mt-2">
+            Explore our latest handcrafted collections.
+          </p>
+        </div>
+
+        <Link href="/products" className="font-semibold text-emerald-600 hover:text-emerald-800">
+          View All Products →
+        </Link>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default FeaturedProducts;
