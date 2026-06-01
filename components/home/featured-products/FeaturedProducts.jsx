74f10b0cf80b7b05059e84dbc92ac80c8e37fdc6
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-01 07:47:59 +0600

    change css

--- components/home/featured-products/FeaturedProducts.jsx
+++ components/home/featured-products/FeaturedProducts.jsx
@@ -9,8 +9,8 @@ const FeaturedProducts = async () => {
     <section className="max-w-7xl mx-auto px-4 md:px-8 py-12">
       <div className="flex items-center justify-between mb-8">
         <div>
-          <h2 className="text-3xl font-bold text-emerald-800">New Products</h2>
-          <p className="text-gray-500 mt-2">
+          <h2 className="text-2xl md:text-3xl font-bold text-emerald-800">New Products</h2>
+          <p className="text-gray-500 max-w-50 md:max-w-2xl mt-2">
             Explore our latest handcrafted collections.
           </p>
         </div>
@@ -20,7 +20,7 @@ const FeaturedProducts = async () => {
         </Link>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard key={product._id} product={product} />
         ))}
