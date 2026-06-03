95bbc2392f274dadd312f1510f0ad19572965afd
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-03 13:14:14 +0600

    change image folder structure

--- data/heroData.js
+++ data/heroData.js
@@ -1,27 +1,26 @@
 export const heroSlides = [
   {
     id: 1,
-    image: "/images/hero/hero1.png",
+    image: "/assets/images/hero/hero1.png",
     title: "Handmade by Nature Chosen for You",
     desc: "Strong and beautiful, just like nature.",
     buttonText: "All Products",
-    linkUrl: "/products"
+    linkUrl: "/products",
   },
   {
     id: 2,
-    image: "/images/hero/hero2.png",
+    image: "/assets/images/hero/hero2.png",
     title: "Decor Your Dream Space",
     desc: "Explore beautiful bamboo & cane handicrafts, made with care , chosen for you.",
     buttonText: "View Gifts",
-    linkUrl: "products?category=gifts-crafts"
-    
+    linkUrl: "products?category=gifts-crafts",
   },
   {
     id: 3,
-    image: "/images/hero/hero3.png",
+    image: "/assets/images/hero/hero3.png",
     title: "Premium Jute Collection",
     desc: "Minimal, aesthetic and natural lifestyle.",
     buttonText: "Explore Jute",
-    linkUrl: "/products"
+    linkUrl: "/products",
   },
 ];
