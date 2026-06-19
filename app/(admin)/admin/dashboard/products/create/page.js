12a649470bb15d643d2731233c6f16c1ebf1170d
Nazmul Kabir Rifat <nk.rifat76@gmail.com>
2026-06-19 19:28:36 +0600

    update

--- app/(admin)/admin/dashboard/products/create/page.js
+++ app/(admin)/admin/dashboard/products/create/page.js
@@ -1,4 +1,5 @@
 import ProductForm from "@/components/admin/products/add-product/ProductForm";
+import Link from "next/link";
 
 export const metadata = { title: "Add New Product – Ponno Admin" };
 
